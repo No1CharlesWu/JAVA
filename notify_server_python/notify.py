603172866f6261e3e1f7f9678c367f44c73012ae
No1ChalresWu <No1CharlesWu@163.com>
2017-12-20 23:27:52 +0800

    修改成服务器IP。调试可用。

--- notify_server_python/notify.py
+++ notify_server_python/notify.py
@@ -29,7 +29,7 @@ class NotifyAgent(object):
             # 监听端口:
             # self.socket.bind(('127.0.0.1', 9999))
             # self.socket.bind(('10.70.3.30', 9999))
-            self.socket.bind(('106.14.193.60', 9999))
+            self.socket.bind(('0.0.0.0', 9999))
             self.socket.listen(10)
             print('Waiting for connection...')
             while True:
